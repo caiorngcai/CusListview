f527707845b7ef09ae80bf5ba7616aa6fa307ab1
cairongcai <1419156712@qq.com>
2017-06-06 01:06:43 +0800

    完成

--- app/src/main/java/com/cairongcai/cuslistview/MainActivity.java
+++ app/src/main/java/com/cairongcai/cuslistview/MainActivity.java
@@ -1,13 +1,112 @@
 package com.cairongcai.cuslistview;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.cairongcai.cuslistview.view.RefreshListview;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RefreshListview cus_listview;
+    private ArrayList<String> datalist;
+    private Myadapter myadapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);//没有标头,必须在加载页面前调用，不然程序调用
+            setContentView(R.layout.activity_main);
+            cus_listview = (RefreshListview) findViewById(R.id.cus_listview);
+         cus_listview.setRefreshListener(new RefreshListview.onRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Thread()
+                {
+                    @Override
+                    public void run() {
+                        SystemClock.sleep(2000);
+                        datalist.add(0,"我是下拉出来的数据");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                myadapter.notifyDataSetChanged();
+                                cus_listview.onRefreshComplete();
+                            }
+                        });
+                    }
+                }.start();
+
+            }
+
+            @Override
+            public void onLoadmore() {
+                new Thread()
+                {
+                    @Override
+                    public void run() {
+                        SystemClock.sleep(2000);
+                        datalist.add("我是加载出来的数据1");
+                        datalist.add("我是加载出来的数据2");
+                        datalist.add("我是加载出来的数据3");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                myadapter.notifyDataSetChanged();
+                                cus_listview.onRefreshComplete();
+                            }
+                        });
+                    }
+
+                }.start();
+
+            }
+        });
+        datalist = new ArrayList<String>();
+        for (int i = 0; i < 30; i++) {
+            datalist.add("这是一条listview数据");
+        }
+        myadapter = new Myadapter();
+        cus_listview.setAdapter(myadapter);
+        }
+
+
+
+    class Myadapter extends BaseAdapter
+    {
+
+        @Override
+        public int getCount() {
+            return datalist.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return datalist.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView=new TextView(parent.getContext());
+            textView.setText(datalist.get(position));
+            textView.setTextSize(18f);
+            textView.setTextColor(Color.BLACK);
+
+            return textView;
+        }
     }
 }
--- app/src/main/java/com/cairongcai/cuslistview/view/RefreshListview.java
+++ app/src/main/java/com/cairongcai/cuslistview/view/RefreshListview.java
@@ -1,24 +1,242 @@
 package com.cairongcai.cuslistview.view;
 
 
+import java.text.SimpleDateFormat;
+
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.cairongcai.cuslistview.R;
 
 /**
  * Created by HY-IT on 2017/6/5.
  */
 
-public class RefreshListview extends View {
+public class RefreshListview extends ListView implements OnScrollListener {
+
+    public static final int PULL_TO_REFRESH=1;
+    public static final int RELEASE_REFRESH=2;
+    public static final int REFRESHING=3;
+    private int currentstate=PULL_TO_REFRESH;
+    private View mViewHeader;
+    private View mArrowView;
+    private ProgressBar pb;
+    private TextView tv_title;
+    private TextView mLastRefreshTime;
+    private int mHeaderViewHeight;
+    private View mViewFoot;
+    private int mFootViewHeight;
+    private RotateAnimation rotateUpAnim;
+    private RotateAnimation rotateDownAnim;
+    private float downY;
+    private float moveY;
+    private onRefreshListener mListener;
+    private boolean isLoadingMore;
     public RefreshListview(Context context) {
         super(context);
+        init();
+
     }
 
     public RefreshListview(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
+
     }
 
-    public RefreshListview(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public RefreshListview(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+
+    }
+    private void init() {
+        initHeaderView();
+        initFootView();
+        initAnimation();
+        setOnScrollListener(this);//注册屏幕滚动监听事件
+    }
+
+    /**
+     * 初始化动画的方法--向上反转，向下反转
+     */
+    private void initAnimation() {
+        rotateUpAnim = new RotateAnimation(0f, -180f
+                , Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        rotateUpAnim.setDuration(300);
+        rotateUpAnim.setFillAfter(true);
+        rotateDownAnim = new RotateAnimation(-180f, -360f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        rotateDownAnim.setDuration(300);
+        rotateDownAnim.setFillAfter(true);
+    }
+
+    /**
+     * 初始化脚布局的方法，在加载listview之前要先加载
+     */
+    private void initFootView() {
+        mViewFoot = View.inflate(getContext(), R.layout.layout_footer_list, null);
+       mViewFoot.measure(0,0);
+        mFootViewHeight = mViewFoot.getMeasuredHeight();
+        mViewFoot.setPadding(0,-mFootViewHeight,0,0);
+        addFooterView(mViewFoot);
+    }
+
+    /**
+     * 初始化头布局的方法
+     */
+    private void initHeaderView() {
+        mViewHeader = View.inflate(getContext(), R.layout.layout_list_header, null);
+        mArrowView = mViewHeader.findViewById(R.id.iv_arrow);
+        pb = (ProgressBar) mViewHeader.findViewById(R.id.pb);
+        tv_title = (TextView) mViewHeader.findViewById(R.id.tv_title);
+        mLastRefreshTime = (TextView) mViewHeader.findViewById(R.id.tv_desc_last_refresh);
+        mViewHeader.measure(0,0);
+        mHeaderViewHeight = mViewHeader.getMeasuredHeight();
+        mViewHeader.setPadding(0,-mHeaderViewHeight,0,0);
+        addHeaderView(mViewHeader);//在设置适配器之前就要执行加载头布局方法
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+       switch (ev.getAction())
+       {
+           case MotionEvent.ACTION_DOWN:
+               downY = ev.getY();
+               break;
+           case MotionEvent.ACTION_MOVE:
+               moveY = ev.getY();
+               if(currentstate==REFRESHING)
+               {
+                   return super.onTouchEvent(ev);
+               }
+               float offset=moveY-downY;
+               if(offset>0&&getFirstVisiblePosition()==0)
+               {
+                   int paddingTop= (int) (offset-mHeaderViewHeight);
+                   mViewHeader.setPadding(0,paddingTop,0,0);
+                   if(paddingTop>=0&&currentstate!=RELEASE_REFRESH)
+                   {
+                       currentstate=RELEASE_REFRESH;
+                       updateHeader();
+                   }else if(offset<0&&currentstate!=PULL_TO_REFRESH)
+                   {
+                       currentstate=PULL_TO_REFRESH;
+                       updateHeader();
+                   }
+                   return true;
+               }
+               break;
+           case MotionEvent.ACTION_UP:
+               if(currentstate==PULL_TO_REFRESH)
+               {
+                   mViewHeader.setPadding(0,-mHeaderViewHeight,0,0);
+               }else if(currentstate==RELEASE_REFRESH)
+               {
+                   mViewHeader.setPadding(0,0,0,0);
+                   currentstate=REFRESHING;
+                   updateHeader();
+               }
+
+               break;
+           default:
+               break;
+
+       }
+        return super.onTouchEvent(ev);
+    }
+
+    private void updateHeader() {
+        switch (currentstate)
+        {
+            case PULL_TO_REFRESH:
+                mArrowView.setAnimation(rotateDownAnim);
+                tv_title.setText("下拉刷新");
+                break;
+            case REFRESHING:
+                mArrowView.clearAnimation();
+                mArrowView.setVisibility(INVISIBLE);
+                pb.setVisibility(VISIBLE);
+                tv_title.setText("正在刷新中");
+                //TODO 这里要调用监听器刷新网络数据,接口
+                if(mListener!=null)
+                {
+                    mListener.onRefresh();
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+    public void onRefreshComplete()
+    {
+        if(isLoadingMore)
+        {
+            mViewFoot.setPadding(0,-mFootViewHeight,0,0);
+            isLoadingMore=false;
+        }else {
+            currentstate=PULL_TO_REFRESH;
+            tv_title.setText("下拉刷新");
+            mViewHeader.setPadding(0,-mHeaderViewHeight,0,0);
+            pb.setVisibility(INVISIBLE);
+            mArrowView.setVisibility(VISIBLE);
+            String time=getTime();
+            mLastRefreshTime.setText(time);
+        }
+
+    }
+
+    public String getTime() {
+        long currentTimeMillis=System.currentTimeMillis();
+        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(currentTimeMillis);
+    }
+
+    public interface onRefreshListener
+    {
+        void onRefresh();
+        void onLoadmore();
+    }
+    public void setRefreshListener(onRefreshListener mListener)
+    {
+        this.mListener=mListener;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if(isLoadingMore)
+        {
+            return;
+        }else if(scrollState==SCROLL_STATE_IDLE&&getLastVisiblePosition()>=(getCount()-1))
+        {
+            isLoadingMore=true;
+            mViewFoot.setPadding(0,0,0,0);
+            setSelection(getCount());
+            if(mListener!=null)
+            {
+                mListener.onLoadmore();
+            }
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
     }
 }
+
+
+
