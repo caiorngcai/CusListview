50d778337d75e6a7ec00b95ee83c188d197ab994
cairongcai <1419156712@qq.com>
2017-06-05 20:22:26 +0800

    第一次提交

---
+++ app/src/main/java/com/cairongcai/cuslistview/view/RefreshListview.java
@@ -0,0 +1,24 @@
+package com.cairongcai.cuslistview.view;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by HY-IT on 2017/6/5.
+ */
+
+public class RefreshListview extends View {
+    public RefreshListview(Context context) {
+        super(context);
+    }
+
+    public RefreshListview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RefreshListview(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
